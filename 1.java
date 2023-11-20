497d05833cf27decc940ceb951fec6c62a93848a
vscharan <vscharan@gmail.com>
2023-11-20 11:47:35 +0000

    java files pushing to java branch

---
+++ 1.java
@@ -0,0 +1,43 @@
+public class vcm {
+    public static void main(String[] args) {
+        System.out.println("****************************");
+        System.out.println("Hello, vcm!");
+    }
+}
+
+
+public class vcm1 {
+    public static void main(String[] args) {
+        System.out.println("welcome vcm");
+    }
+}
+
+
+
+public class vjs {
+    public static void main(String[] args) {
+        System.out.println("****************************");
+        System.out.println("Hello, vjs!");
+    }
+}
+
+
+public class vjs1 {
+    public static void main(String[] args) {
+        System.out.println("welcome vjs");
+    }
+}
+
+public class naga {
+    public static void main(String[] args) {
+        System.out.println("****************************");
+        System.out.println("Hello, naga!");
+    }
+}
+
+
+public class naga1 {
+    public static void main(String[] args) {
+        System.out.println("welcome naga");
+    }
+}
